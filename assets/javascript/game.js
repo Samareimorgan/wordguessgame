2699e7ddf2d7e9dcba653a4274849138332e982f
Samantha Johnston <samareimorgan@gmail.com>
2018-08-03 15:32:12 -0600

    Add Arrays

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//..Level Arrays..//
+var mildLevel=["Hot Tamales", "Red Hots", "Yellow", "Ghost", "Red", "Bell", "Cumin", "Chili Powder", "Cajun Seasoning", "Paprika", "Cinnamon", "Sahara", "Arabian", "Kalahari", "Mojave", "Kilauea", "Mount Saint Helens", "Yellowstone"]
+
+var mediumLevel=["Cinnamon Discs", "Jolly Rancher Fire Stick", "Cinnamon Bears", "Atomic Fireballs", "Habanero Orange", "Serrano", "Cayenne", "Turmeric", "Coriander", "Ground Ginger", "Syrian", "Great Victoria", "Simpson", "Mauna Loa", "Krakatoa", "Mount Vesuvius", "Mount Pelée"]
+
+var spicyLevel=["Tabasco Jelly Belly Jelly Beans", "Sweet Heat Skittles", "Carolina Reaper", "Devil's Tongue Red", "Hot Paper Lantern", "Scotch Bonnet", "Cardamom", "Sumac", "Juniper Berry", "Sonoran", "Thar", "Chihuahuan", "Havre", "Olympus Mons", "Parícutin"]
+
+var extraSpicyLevel=["Mango Chile Barrels", "Wasabi Candy Nuggets", "Naga Viper", "Fatalii", "Bahamian Bird", "Amchur Powder", "Galangal", "Achiote", "Nigella Seeds", "Registan", "Dasht-e Margo", "Novarupta", "Shishaldin"]
+
+//..Category Arrays..//
+var candy=["Hot Tamales", "Red Hots","Cinnamon Discs", "Jolly Rancher Fire Stick", "Cinnamon Bears",  "Atomic Fireballs", "Tabasco Jelly Belly Jelly Beans", "Sweet Heat Skittles", "Mango Chile Barrels"]
+
+var chiliPepper=["Jalapeno","Bell", "Red", Ghost, Yellow, Habanero Orange, Serrano, Carolina Reaper, Devil's Tongue Red, Hot Paper Lantern, Scotch Bonnet, Naga Viper, Fatalii, Bahamian Bird"]
