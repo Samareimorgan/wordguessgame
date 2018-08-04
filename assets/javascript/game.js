8d9e31d578e2bbc68a63d64eb384748d9990cb19
Samantha Johnston <samareimorgan@gmail.com>
2018-08-03 19:09:02 -0600

    Arrange CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 //..Level Arrays..//
-var mildLevel=["Hot Tamales", "Red Hots", "Yellow", "Ghost", "Red", "Bell", "Cumin", "Chili Powder", "Cajun Seasoning", "Paprika", "Cinnamon", "Sahara", "Arabian", "Kalahari", "Mojave", "Kilauea", "Mount Saint Helens", "Yellowstone"]
+var mildLevel=["Hot Tamales", "Red Hots", "Yellow", "Ghost", "Red", "Bell", "Cumin", "Chili Powder", "Cajun Seasoning", "Paprika", "Cinnamon", "Sahara", "Arabian", "Kalahari", "Mojave", "Kilauea", "Mount Saint Helen's", "Yellowstone"]
 
-var mediumLevel=["Cinnamon Discs", "Jolly Rancher Fire Stick", "Cinnamon Bears", "Atomic Fireballs", "Habanero Orange", "Serrano", "Cayenne", "Turmeric", "Coriander", "Ground Ginger", "Syrian", "Great Victoria", "Simpson", "Mauna Loa", "Krakatoa", "Mount Vesuvius", "Mount Pelée"]
+var mediumLevel=["Cinnamon Discs", "Jolly Rancher Fire Stick", "Cinnamon Bears", "Atomic Fireballs", "Habanero Orange", "Serrano", "Cayenne", "Turmeric", "Coriander", "Ground Ginger", "Syrian", "Great Victoria", "Simpson", "Mauna Loa", "Krakatoa", "Mount Vesuvius", "Mount Pelee"]
 
 var spicyLevel=["Tabasco Jelly Belly Jelly Beans", "Sweet Heat Skittles", "Carolina Reaper", "Devil's Tongue Red", "Hot Paper Lantern", "Scotch Bonnet", "Cardamom", "Sumac", "Juniper Berry", "Sonoran", "Thar", "Chihuahuan", "Havre", "Olympus Mons", "Parícutin"]
 
@@ -10,4 +10,12 @@ var extraSpicyLevel=["Mango Chile Barrels", "Wasabi Candy Nuggets", "Naga Viper"
 //..Category Arrays..//
 var candy=["Hot Tamales", "Red Hots","Cinnamon Discs", "Jolly Rancher Fire Stick", "Cinnamon Bears",  "Atomic Fireballs", "Tabasco Jelly Belly Jelly Beans", "Sweet Heat Skittles", "Mango Chile Barrels"]
 
-var chiliPepper=["Jalapeno","Bell", "Red", Ghost, Yellow, Habanero Orange, Serrano, Carolina Reaper, Devil's Tongue Red, Hot Paper Lantern, Scotch Bonnet, Naga Viper, Fatalii, Bahamian Bird"]
+var chiliPepper=["Jalapeno","Bell", "Red", "Ghost", "Yellow", "Habanero Orange", "Serrano", "Carolina Reaper", "Devil's Tongue Red", "Hot Paper Lantern", "Scotch Bonnet", "Naga Viper", "Fatalii", "Bahamian Bird"]
+
+var deserts=["Sahara", "Arabian", "Kalahari", "Mojave", "Syrian", "Great Victoria", "Simpson", "Sonoran", "Thar", "Chihuahuan", "Registan", "Dasht-e Margo"]
+
+var spices=["Cajun Seasoning", "Chili Powder", "Paprika", "Cumin", "Cinnamon", "Cayenne", "Turmeric", "Coriander", "Ground Ginger", "Cardamom", "Sumac", "Juniper Berry", "Amchur Powder", "Galangal", "Achiote", "Nigella Seeds"]
+
+var volcanoes=["Yellowstone", "Kilauea", "Mount Saint Helens", "Mauna Loa", "Krakatoa", "Mount Vesuvius", "Olympus Mons",
+"Mount Pelee" , "Havre", "Paricutin", "Novarupta", "Shishaldin"]
+
