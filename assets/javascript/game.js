66c10d564c773766ed8e30e4b80257dca1cab471
Samantha Johnston <samareimorgan@gmail.com>
2018-08-04 08:57:23 -0600

    Add pseudocode in Javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,9 @@ var spicyLevel=["Tabasco Jelly Belly Jelly Beans", "Sweet Heat Skittles", "Carol
 
 var extraSpicyLevel=["Mango Chile Barrels", "Wasabi Candy Nuggets", "Naga Viper", "Fatalii", "Bahamian Bird", "Amchur Powder", "Galangal", "Achiote", "Nigella Seeds", "Registan", "Dasht-e Margo", "Novarupta", "Shishaldin"]
 
+// Array of the Various Levels //
+var levelsArray=["mildLevel", "mediumLevel","spicyLevel", "extraSpicyLevel"]
+
 //..Category Arrays..//
 var candy=["Hot Tamales", "Red Hots","Cinnamon Discs", "Jolly Rancher Fire Stick", "Cinnamon Bears",  "Atomic Fireballs", "Tabasco Jelly Belly Jelly Beans", "Sweet Heat Skittles", "Mango Chile Barrels"]
 
@@ -19,5 +22,26 @@ var spices=["Cajun Seasoning", "Chili Powder", "Paprika", "Cumin", "Cinnamon", "
 var volcanoes=["Yellowstone", "Kilauea", "Mount Saint Helens", "Mauna Loa", "Krakatoa", "Mount Vesuvius", "Olympus Mons",
 "Mount Pelee" , "Havre", "Paricutin", "Novarupta", "Shishaldin"]
 
-var mildLevel=$()
+// 1. Press a heat level (difficulty) to get started
+// 2. On Click of Any heat Level
+// 3. goes through that array level and randomly picks a word
+// 4. After word is choice, it goes through the Category arrays until the word is matched.
+// 5. Once the word is matched the script then "activates" the category on the UI
+// 6. Also, once the word is selected, letter spaces are created (appended) to the Wordbox with the length of letters in the selected word.
+// 7. Then, the words " Please select a letter " comes on the screen
+// 8. On key event (letter selection), the script goes through the chosen word in the wordbox to see if that letter matches any in the word. 
+//  9. If a letter matches, then it highlights that letter in the letter box Green.
+//         Also if a letter matches, then it places it on the appropriate line in the wordbox. 
+//         Also it adds it to the Letters Guessed area 
+//         Also it decreases the guesses left by 1
+// 10. If a letter does not match, then it highlights that letter in the letter box red. 
+//           Also, it adds it to the guessed letters areas
+//           It decreases the guesses left by 1
+// Total guesses for the word 12
+// If all guesses are used and the word has not been solved by User - the User loses 
+// If all letters are guessed in the word, they win the game.  And the Win goes up by 1 
+// If the User presses the Solution Button - they forfeit the game and Losses increases by 1
+// On click of the Solution Button - The word in the wordbox appears
+// Begin New Game Button 
+    // When clicked on, it resets the game and the wins and losses
 
